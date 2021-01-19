471eb444ce23fafd98178752d9003a8e620e1d39
yurayura420 <tatu19970625@gmail.com>
2021-01-19 10:56:50 +0900

    既読機能のフロント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   
   def create
     Post.create(content: params[:content])
-    redirect to root_path
+    redirect_to root_path
   end
 
   def checked
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
     else  
       post.update(checked: true)
     end
-    item = Post.find(params[:item])
+    item = Post.find(params[:id])
     render json: {post: item}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  get 'posts', to: 'posts#checked'
-  post 'posts/:id', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
+  post 'posts', to: 'posts#create'
 end
